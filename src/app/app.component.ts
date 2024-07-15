ffd456163c8ae32414324024e9a34bb1e8c4b2b8
Titah Mostafa <etitah-stagiaire@humanbooster.com>
2024-07-15 14:32:43 +0200

    articles Componenat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { SectionIntroComponent } from './section-intro/section-intro.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { ArticlesComponent } from './articles/articles.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,SectionIntroComponent,CarouselComponent],
+  imports: [RouterOutlet,HeaderComponent,SectionIntroComponent,CarouselComponent,ArticlesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
