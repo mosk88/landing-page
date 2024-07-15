06177063b2007a6b176a06d9dd3b17a672d1ceb2
Titah Mostafa <etitah-stagiaire@humanbooster.com>
2024-07-15 14:49:56 +0200

    footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { SectionIntroComponent } from './section-intro/section-intro.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { ArticlesComponent } from './articles/articles.component';
 import { TestiMonyComponent } from './testi-mony/testi-mony.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,SectionIntroComponent,CarouselComponent,ArticlesComponent,TestiMonyComponent],
+  imports: [RouterOutlet,HeaderComponent,SectionIntroComponent,CarouselComponent,ArticlesComponent,TestiMonyComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
