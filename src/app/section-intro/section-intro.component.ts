f0d1e03338c908f87cd09e784b239211165869d8
Titah Mostafa <etitah-stagiaire@humanbooster.com>
2024-07-15 13:56:35 +0200

    section introdiction with video componenat

---
+++ src/app/section-intro/section-intro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-intro',
+  standalone: true,
+  imports: [],
+  templateUrl: './section-intro.component.html',
+  styleUrl: './section-intro.component.css'
+})
+export class SectionIntroComponent {
+title = 'landingPage';
+}
