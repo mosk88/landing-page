44864797424d5b2626df6d8e19831a8a10bb47fa
Titah Mostafa <etitah-stagiaire@humanbooster.com>
2024-07-15 14:05:56 +0200

    carousel Componenat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { SectionIntroComponent } from './section-intro/section-intro.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,SectionIntroComponent],
+  imports: [RouterOutlet,HeaderComponent,SectionIntroComponent,CarouselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
